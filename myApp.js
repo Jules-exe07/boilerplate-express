571de54d7a884932951a313be004c28caeaa3643
Jules-exe07 <79948125+Jules-exe07@users.noreply.github.com>
2021-03-02 19:09:00 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-
-
+app.get('/json', (req, res) => {
+ const message = (process.env.MESSAGE_STYLE === 'uppercase') ? 'HELLO JSON' : 'Hello json';
+ res.json({'message':'hello json'});
+});
 
 
 
